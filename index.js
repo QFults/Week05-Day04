fedc890ada66e339270c854970b1c298813f30d9
Quinton Fults <quinton.fults.work@gmail.com>
2021-03-05 11:29:44 -0800

    code

---
+++ index.js
@@ -0,0 +1,57 @@
+// function User (name, email, username, phone) {
+//   this.name = name
+//   this.email = email
+//   this.username = username
+//   if (phone) {
+//     this.phone = phone
+//   } else {
+//     this.phone = 1111111111
+//   }
+// }
+
+// function Student (name, email, username, phone, grade, gpa) {
+//   User.call(this, name, email, username, phone)
+//   this.grade = grade
+//   this.gpa = gpa
+// }
+
+// function Teacher (name, email, username, phone, subject) {
+//   User.call(this, name, email, username, phone)
+//   this.subject = subject
+// }
+
+// const student = new Student('John Doe', 'johndoe@gmail.com', 'johndoe', 1234567890, 11, 4.0)
+// const teacher = new Teacher('James Doe', 'jamesDoe@gmail.com', 'jamesdoe', null,'Math')
+
+// console.log(student)
+// console.log(teacher)
+
+
+class User {
+  constructor (name, email, username) {
+    this.name = name
+    this.email = email
+    this.username = username
+  }
+}
+
+class Student extends User {
+  constructor (name, email, username, grade, gpa) {
+    super(name, email, username)
+    this.grade = grade
+    this.gpa = gpa
+  }
+}
+
+class Teacher extends User {
+  constructor (name, email, username, subject) {
+    super(name, email, username)
+    this.subject = subject
+  }
+}
+
+const student = new Student('John Doe', 'johndoe@gmail.com', 'johndoe', 11, 4.0)
+const teacher = new Teacher('James Doe', 'jamesDoe@gmail.com', 'jamesdoe', 'Math')
+
+console.log(student)
+console.log(teacher)
